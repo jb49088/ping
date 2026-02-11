e3bfa73399c973e5a6f656de1803a14c926ad9d8
Justin Braun <justinbraun335@gmail.com>
2026-02-10 20:24:45 -0500

    feat: initial implementation of checksum

--- ping.py
+++ ping.py
@@ -23,14 +23,25 @@ import struct
 ICMP_ECHO_REQUEST = 8
 
 
+def checksum(packet):
+    total = 0
+    # Sum 16 bit words
+    for i in range(0, len(packet), 2):
+        word = (packet[i] << 8) + packet[i + 1] if i + 1 < len(packet) else 0
+        total += word
+
+    return total
+
+
 def ping():
     dest = "www.google.com"
-    header = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0, 5000, 999, 1)
+    header = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0, 0, 1, 1)
     data = b"\x00" * 56
-    packet = header + data
+    chksum = checksum(header + data)
+    header = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0, chksum, 1, 1)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    sock.sendto(packet, (dest, 1))
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    # sock.sendto(packet, (dest, 1))
 
 
 if __name__ == "__main__":
